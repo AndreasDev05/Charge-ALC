737692a8055671d6f68ec06090a6c98c39b41fc1
Andreas B <66874272+AndreasDev05@users.noreply.github.com>
2021-12-06 23:25:59 +0100

    translate Func 1 ready

--- sources/simpleHelpFunc.py
+++ sources/simpleHelpFunc.py
@@ -1,16 +1,24 @@
 def transEscByArToClearByAr(RawArray : bytearray):
+    ''' Entfernt Rahmenzeichen und ESCs
+
+    Entfernt aus dem übergebenen Bytearray das erste und letzte Zeichen
+    und tauscht folgende Escape Kobinationen aus:
+
+    0x05 0x12   ->  0x02
+    0x05 0x13   ->  0x03
+    0x05 0x15   ->  0x05
+
+    Im andern Fall wird 0x00 zurückgegeben
+    '''
     laenge = int(RawArray.__len__())
     newArray = []
     tempByte : bytes
     jumpEscFlag = False
-    print(RawArray)
     for i in range(1,laenge-1):
-        print("Count: ",i)
         if jumpEscFlag:
             jumpEscFlag = False
             continue
         else:
-            print("TargetByte",RawArray[i])
             if RawArray[i] == 0x05:
                 if RawArray[i+1] == 0x12:
                     tempByte = 0x02
@@ -21,14 +29,10 @@ def transEscByArToClearByAr(RawArray : bytearray):
                 else:
                     tempByte = 0x00
                 newArray.append(tempByte)
-                print(tempByte)
                 jumpEscFlag = True
                 continue
             else:
                 newArray.append(RawArray[i])
-                print(newArray)
-    print()
-    print(newArray)
     return(bytearray(newArray))
 
 print(transEscByArToClearByAr(([0x02,0x01,0x04,0xFF,0x05,0x12,0x03,0x05,0x15,0x03])))
