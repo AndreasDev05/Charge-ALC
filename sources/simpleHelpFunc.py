1824f440a92bbff5c5712990e2c173dbeb283f16
Andreas B <66874272+AndreasDev05@users.noreply.github.com>
2021-12-07 22:52:17 +0100

    small error handling

--- sources/simpleHelpFunc.py
+++ sources/simpleHelpFunc.py
@@ -1,8 +1,13 @@
+import sys
+
+class byteArrayToShort(Exception):
+    pass
+
 def transEscByArToClearByAr(RawArray : bytearray):
     ''' Entfernt Rahmenzeichen und ESCs
 
     Entfernt aus dem übergebenen Bytearray das erste und letzte Zeichen
-    und tauscht folgende Escape Kobinationen aus:
+    und tauscht folgende Escape Kombinationen aus:
 
     0x05 0x12   ->  0x02
     0x05 0x13   ->  0x03
@@ -11,11 +16,13 @@ def transEscByArToClearByAr(RawArray : bytearray):
     Im andern Fall wird 0x00 zurückgegeben
     '''
     laenge = int(RawArray.__len__())
+    if laenge < 3:
+        raise byteArrayToShort("Bytearry to short, min. 3")
     newArray = []
     tempByte : bytes
     jumpEscFlag = False
     for i in range(1,laenge-1):
-        if jumpEscFlag:
+        if jumpEscFlag:  # jump over, it is the second char of escape sequence
             jumpEscFlag = False
             continue
         else:
@@ -35,4 +42,44 @@ def transEscByArToClearByAr(RawArray : bytearray):
                 newArray.append(RawArray[i])
     return(bytearray(newArray))
 
-print(transEscByArToClearByAr(([0x02,0x01,0x04,0xFF,0x05,0x12,0x03,0x05,0x15,0x03])))
+def transByArToEscByAr(RawArray : bytearray):
+    ''' Fügt Rahmenzeichen und ESCs ein
+
+    Fügt in das übergebenen Bytearray die Rahmenzeichen ein
+    und tauscht folgende Escape Kombinationen aus:
+
+    0x02    ->  0x05 0x12
+    0x03    ->  0x05 0x13
+    0x05    ->  0x05 0x15
+
+    '''
+    laenge = int(RawArray.__len__())
+    if laenge < 1:
+        raise byteArrayToShort("Bytearry to short, min. 1")
+    newArray = [0x02]
+    for i in range(0,laenge):
+        if RawArray[i] == 0x02:
+            newArray.append(0x05)
+            newArray.append(0x12)
+        elif RawArray[i] == 0x03:
+            newArray.append(0x05)
+            newArray.append(0x13)
+        elif RawArray[i] == 0x05:
+            newArray.append(0x05)
+            newArray.append(0x15)
+        else:
+            newArray.append(RawArray[i])
+    newArray.append(0x03)
+    return(bytearray(newArray))
+
+# 
+if __name__ == "__main__":
+  
+    print(transEscByArToClearByAr(([0x02,0x01,0x04,0xFF,0x05,0x12,0x03,0x05,0x15,0x03])))
+
+    try:
+        print(transEscByArToClearByAr([0x02,0x03]))
+    except byteArrayToShort:
+        print("Der übergebene String ist zu kurz!")
+
+    print(transByArToEscByAr([0x01,0x03,0x04,0xFF]))
